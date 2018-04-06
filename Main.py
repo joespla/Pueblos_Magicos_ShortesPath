e2ea6a194a242feec24c0091ac24220a99e90899
Jorge Espinosa Lara <joespla@Jorges-MacBook-Pro.local>
2018-04-06 15:42:32 -0500

    Se agrego Dijkstra

--- Main.py
+++ Main.py
@@ -103,10 +103,81 @@ def api():
         print(lugar[orden[i]] + "\n")
 
 
+def dijkstra(G, a, z):
+    """
+    Algoritmo de Dijkstra
+    Determina el camino mas corto entre los vertices 'a' y 'z' de un
+    grafo ponderado y conexo 'G'.
+    https://developers.google.com/maps/documentation/distance-matrix/get-api-key
+    """
+    assert a in G
+    assert z in G
+
+    # Definicion de infinito como un valor mayor
+    # al doble de suma de todos los pesos
+    Inf = 0
+    for u in G:
+        for v, w in G[u]:
+            Inf += w
+
+    # Inicializacion de estructuras auxiliares:
+    #  L: diccionario vertice -> etiqueta
+    #  S: conjunto de vertices con etiquetas temporales
+    #  A: vertice -> vertice previo (en camino longitud minima)
+    L = dict([(u, Inf) for u in G])  # py3: L = {u:Inf for u in G}
+    L[a] = 0
+    S = set([u for u in G])  # py3: S = {u for u in G}
+    A = {}
+
+    # Funcion auxiliar, dado un vertice retorna su etiqueta
+    # se utiliza para encontrar el vertice the etiqueta minima
+    def W(v):
+        return L[v]
+
+    # Iteracion principal del algoritmo de Dijkstra
+    while z in S:
+        u = min(S, key=W)
+        S.discard(u)
+        for v, w in G[u]:
+            if v in S:
+                if L[u] + w < L[v]:
+                    L[v] = L[u] + w
+                    A[v] = u
+
+    # Reconstruccion del camino de longitud minima
+    P = []
+    u = z
+    while u != a:
+        P.append(u)
+        u = A[u]
+    P.append('a')
+    P.reverse()
+
+    # retorna longitud minima y camino de longitud minima
+    return L[z], P
+
+
 if __name__ == '__main__':
-    api()
+    from pprint import pprint
+
+    #api()
+    G1 = {  # Rosen, Figura 4 (pp. 559)
+        'a': [('b', 4), ('c', 2)],
+        'b': [('a', 4), ('c', 1), ('d', 5)],
+        'c': [('a', 2), ('b', 1), ('d', 8), ('e', 10)],
+        'd': [('b', 5), ('c', 8), ('e', 2), ('z', 6)],
+        'e': [('c', 10), ('d', 2), ('z', 3)],
+        'z': [('d', 6), ('e', 3)],
+    }
+
+    w, p = dijkstra(G1, 'a', 'z')
+    pprint(p)
+    pprint(w)
+
 
 '''
-    La información del API se encuentra en el siguiente link:
-        https://developers.google.com/maps/documentation/distance-matrix/usage-limits
+    - Sólo ocupar 10 nodos/pueblos
+    -
 '''
+
+
