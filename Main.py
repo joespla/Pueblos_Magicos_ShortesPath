11f7f13d764c83490d535c76fbad2bd606fd0480
Jorge Espinosa Lara <joespla@Jorges-MacBook-Pro.local>
2018-04-07 16:48:18 -0500

    Ultimos toques

--- Main.py
+++ Main.py
@@ -1,7 +1,5 @@
 import json
 import requests
-import sys
-import time
 
 
 def pueblos(nombres):
@@ -40,10 +38,8 @@ def api():
         'api_key': api_key
     }
 
-    # Assemble the URL and query the web service
     r = requests.get(base_url, params=payload)
 
-    # Checa el codigo regresado por el servidor HTTP. Sólo funciona con código 200
     if r.status_code != 200:
         print('HTTP status code {} received, program terminated.'.format(r.status_code))
     else:
@@ -58,16 +54,6 @@ def api():
 
         except ValueError:
             print("Error while parsing JSON respone, program KILLED")
-
-    '''
-    for isrc, src in enumerate(x['origin_addresses']):
-        for idst, dst in enumerate(x['destination_addresses']):
-            lugar.append(dst)
-            row = x['rows'][isrc]
-            cell = row['elements'][idst]
-            z = cell['distance']['value']
-            tiempo.append(z)
-    '''
 
 
     places = {
@@ -102,37 +88,23 @@ def api():
 
 
 def dijkstra(G, a, z):
-    """
-    Algoritmo de Dijkstra
-    Determina el camino mas corto entre los vertices 'a' y 'z' de un
-    grafo ponderado y conexo 'G'.
-    https://developers.google.com/maps/documentation/distance-matrix/get-api-key
-    """
+
     assert a in G
     assert z in G
 
-    # Definicion de infinito como un valor mayor
-    # al doble de suma de todos los pesos
     Inf = 0
     for u in G:
         for v, w in G[u]:
             Inf += w
 
-    # Inicializacion de estructuras auxiliares:
-    #  L: diccionario vertice -> etiqueta
-    #  S: conjunto de vertices con etiquetas temporales
-    #  A: vertice -> vertice previo (en camino longitud minima)
     L = dict([(u, Inf) for u in G])  # py3: L = {u:Inf for u in G}
     L[a] = 0
     S = set([u for u in G])  # py3: S = {u for u in G}
     A = {}
 
-    # Funcion auxiliar, dado un vertice retorna su etiqueta
-    # se utiliza para encontrar el vertice the etiqueta minima
     def W(v):
         return L[v]
 
-    # Iteracion principal del algoritmo de Dijkstra
     while z in S:
         u = min(S, key=W)
         S.discard(u)
@@ -142,7 +114,6 @@ def dijkstra(G, a, z):
                     L[v] = L[u] + w
                     A[v] = u
 
-    # Reconstruccion del camino de longitud minima
     P = []
     u = z
     while u != a:
@@ -151,7 +122,6 @@ def dijkstra(G, a, z):
     P.append(a)
     P.reverse()
 
-    # retorna longitud minima y camino de longitud minima
     return L[z], P
 
 
@@ -159,26 +129,4 @@ if __name__ == '__main__':
     from pprint import pprint
 
     api()
-
-
-'''
-    - Sólo ocupar 10 nodos/pueblos
-    -
-'''
-
-'''
-G1 = {  # Rosen, Figura 4 (pp. 559)
-        'a': [('b', 4), ('c', 2)],
-        'b': [('a', 4), ('c', 1), ('d', 5)],
-        'c': [('a', 2), ('b', 1), ('d', 8), ('e', 10)],
-        'd': [('b', 5), ('c', 8), ('e', 2), ('z', 6)],
-        'e': [('c', 10), ('d', 2), ('z', 3)],
-        'z': [('d', 6), ('e', 3)],
-    }
-
-    w, p = dijkstra(G1, 'a', 'z')
-    pprint(p)
-    pprint(w)
-'''
-
 
